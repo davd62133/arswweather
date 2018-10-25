7854bc1f8dc5d02871be359bd421657d4e7f668f
Rafael <luis572>
2018-10-25 18:29:45 -0500

    correccion

--- src/main/resources/static/Cover Template for Bootstrap_files/js/BackEndController.js
+++ src/main/resources/static/Cover Template for Bootstrap_files/js/BackEndController.js
@@ -1,5 +1,5 @@
 async function getClima(ciudad){
-	await Promise.resolve(axios.get('http://localhost:8080/weather/'+ciudad)
+	await Promise.resolve(axios.get('https://parcialarswdavid.herokuapp.com/weather/'+ciudad)
 	.then(async function (response){            
             clima.setClima(response.data);
 	}));
