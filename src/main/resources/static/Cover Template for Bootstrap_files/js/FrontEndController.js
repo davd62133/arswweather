e9c4697f77d7cf54537e584580442e39499d3d3d
Rafael <luis572>
2018-10-25 18:35:25 -0500

    agregar informacion

--- src/main/resources/static/Cover Template for Bootstrap_files/js/FrontEndController.js
+++ src/main/resources/static/Cover Template for Bootstrap_files/js/FrontEndController.js
@@ -21,7 +21,8 @@ async function inicio(){
 function setInfo(){
     var m = "";
     m += "Las coordenadas son: latitud " + clima.getClima().coord.lat+" Longitud "+clima.getClima().coord.lon;
-    m+= "Clima: " + clima.getClima().weather[0].main + " description: " + clima.getClima().weather[0].description;
+    m += " Clima: " + clima.getClima().weather[0].main + " description: " + clima.getClima().weather[0].description;
+    m +=  " Temperatura: " + clima.getClima().main.temp + " Presion: " + clima.getClima().main.pressure + " Humedad: " + clima.getClima().main.humidity; 
     document.getElementById("orders").innerHTML = m;
 }
 
