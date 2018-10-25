790dfbc2529712a58a0d1b842845dab4ba757997
Rafael <luis572>
2018-10-25 18:28:32 -0500

    agregar cache

--- src/main/java/com/arsw/weather/services/impl/OpenWeather.java
+++ src/main/java/com/arsw/weather/services/impl/OpenWeather.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
@@ -26,29 +27,34 @@ public class OpenWeather implements WeatherService{
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String GET_URL = "http://api.openweathermap.org/data/2.5/weather?q=";   
     private static final String GET_URLEND = "&appid=63b8a5baeade1b60dce33b3f847ce983";  
+    private HashMap<String, String> cache = new HashMap();
     
     @Override
     public String weatherInCity(String city) {
-        System.out.println(city);
-        StringBuffer response = new StringBuffer();
-        try {
-            URL obj = new URL(GET_URL + city + GET_URLEND);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("User-Agent", USER_AGENT);            
+        if(cache.containsKey(city)){
+            return cache.get(city);
+        }else{           
+            StringBuffer response = new StringBuffer();
+            try {
+                URL obj = new URL(GET_URL + city + GET_URLEND);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("User-Agent", USER_AGENT);            
                         
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-                String inputLine;                
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-            }            
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(OpenWeather.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(OpenWeather.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return response.toString();
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                    String inputLine;                
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                }            
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(OpenWeather.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(OpenWeather.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            cache.put(city, response.toString());
+            return cache.get(city);
+            }
     }
     
 }
